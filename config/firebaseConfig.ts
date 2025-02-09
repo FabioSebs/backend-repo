368f3aedc08b417ab14ed8f68b00ea6b3eae0d8a
FabioSebs <sebsphotosofficial@gmail.com>
2025-02-09 23:40:48 +0700

    first commit

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.PROJECT_KEY,
+  authDomain: process.env.PROJECT_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.PROJECT_BUCKET,
+  messagingSenderId: process.env.PROJECT_MESSAGE_ID,
+  appId: process.env.PROJECT_APP_ID,
+  measurementId: process.env.PROJECT_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
