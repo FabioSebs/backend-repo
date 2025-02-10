48b7f2855d10761936d38e1f987401a949bcdd43
FabioSebs <sebsphotosofficial@gmail.com>
2025-02-11 01:18:52 +0700

    cors

--- core/app.ts
+++ core/app.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import  dotenv from "dotenv";
+import cors from "cors"
 import { firebaseMiddleware } from "../middleware/firebaseMiddleware.js";
 import userRouter from "../routes/userRoutes.js";
 
 // * config
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // * middlewares
+app.use(cors({
+    origin: "http://localhost:3000", // Allow requests from this origin
+    credentials: true, // Allow cookies if needed
+  }));
 app.use(express.json());
 app.use(firebaseMiddleware)
 
